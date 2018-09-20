4d4d68887544631d20dc7c0223ecdfc7b8faf3eb
jichunMa <jichunma@yangqianguan.com>
2018-09-20 21:07:12 +0800

    half solve number189

---
+++ src/number189/Solution.java
@@ -0,0 +1,24 @@
+package number189;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arrays = {1, 2, 3, 4, 5, 6, 7};
+        for (int i = 0; i < arrays.length; i++) {
+            System.out.println(arrays[i]);
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        if (nums.length == 0){
+            return;
+        }
+        int length = nums.length;
+        int tmp;
+        for (int i = 0; i < length; i++) {
+            
+
+        }
+
+    }
+}
