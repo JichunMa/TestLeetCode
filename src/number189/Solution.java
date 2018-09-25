b347937c0a348dde25b5d0b2bbf9180feb1fdf05
jichunMa <jichunma@yangqianguan.com>
2018-09-25 11:37:38 +0800

    half solve number189

--- src/number189/Solution.java
+++ src/number189/Solution.java
@@ -1,24 +1,28 @@
 package number189;
 
+// TODO: 2018/9/25  需求3种方式实现已经完成(1/3) 
 public class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arrays = {1, 2, 3, 4, 5, 6, 7};
+        int[] arrays = {-1,-100,3,99};
+        solution.rotate(arrays, 2);
         for (int i = 0; i < arrays.length; i++) {
             System.out.println(arrays[i]);
         }
     }
 
     public void rotate(int[] nums, int k) {
-        if (nums.length == 0){
+        int length = nums.length;
+        if (length == 0) {
             return;
         }
-        int length = nums.length;
         int tmp;
-        for (int i = 0; i < length; i++) {
-            
-
+        for (int i = k; i > 0; i--) {
+            tmp = nums[length - 1];
+            for (int j = length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = tmp;
         }
-
     }
 }
