989b8d4bbf9d0f688098aed88aa6e844191e23cd
jichunMa <jichunma@yangqianguan.com>
2018-12-04 18:24:41 +0800

    solve 070

---
+++ src/number070/Solution.java
@@ -0,0 +1,29 @@
+package number070;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.climbStairs(3));
+    }
+
+    public int climbStairs(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        for (int i = 3; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
