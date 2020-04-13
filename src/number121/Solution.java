c0c638c1b14ad58f3a71eb6ca3fe0be125820ef5
jichunMa <jichunma@yangqianguan.com>
2020-04-13 23:09:26 +0800

    买卖股票的最佳时机

--- src/number121/Solution.java
+++ src/number121/Solution.java
@@ -1,11 +1,6 @@
+package number121;
 
 public class Solution {
-
-    public static void main(String[] args) {
-        int[] prices = {7,6,4,3,1};
-        System.out.println(maxProfit(prices));
-    }
-
     public static int maxProfit(int[] prices) {
         if (prices.length < 1) {
             return 0;
@@ -22,5 +17,4 @@ public class Solution {
         }
         return max;
     }
-
 }
