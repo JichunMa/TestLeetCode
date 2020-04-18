cc0b2ecad50a732a819f3e43c23d7dc7f319f607
jichunMa <jichunma@yangqianguan.com>
2020-04-18 23:58:30 +0800

    最后一个单词的长度

--- src/number058/Solution.java
+++ src/number058/Solution.java
@@ -1,17 +1,6 @@
-import number002.ListNode;
-import sun.nio.cs.ArrayEncoder;
-
-import java.util.Arrays;
-import java.util.Stack;
-
+package number058;
 
 public class Solution {
-
-
-    public static void main(String[] args) {
-    }
-
-
     public int lengthOfLastWord(String s) {
         if (s == null) {
             return 0;
@@ -19,14 +8,11 @@ public class Solution {
         if (s.trim().length() == 0) {
             return 0;
         }
-        if (s.indexOf(" ") == -1 || s.indexOf(" ") == s.length() - 1) {
-            return 0;
+        if (s.indexOf(" ") == -1) {
+            return s.length();
         }
 
         String[] strings = s.split(" ");
         return strings[strings.length - 1].length();
-
     }
-
-
 }
