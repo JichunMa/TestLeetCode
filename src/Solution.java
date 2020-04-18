640280d0b6b48e93a1514c8a24687ea0389a7eb4
jichunMa <jichunma@yangqianguan.com>
2020-04-18 22:28:29 +0800

    搜索二维矩阵 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,24 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
 
     public static void main(String[] args) {
 
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.subList(0, 2);
-        System.out.println(list.size());
+
     }
 
-    public static int[] dailyTemperatures(int[] T) {
-        int[] result = new int[T.length];
-        return result;
-    }
 
 
 }
