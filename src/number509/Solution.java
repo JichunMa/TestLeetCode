e941ebe50cbad27aa4947d64bcd7f0e773c50036
jichunMa <jichunma@yangqianguan.com>
2020-04-18 22:39:17 +0800

    斐波那契数

--- src/number509/Solution.java
+++ src/number509/Solution.java
@@ -1,16 +1,6 @@
-import number002.ListNode;
-import sun.nio.cs.ArrayEncoder;
-
-import java.util.Stack;
-
+package number509;
 
 public class Solution {
-
-
-    public static void main(String[] args) {
-
-    }
-
     public int fib(int N) {
         if (N == 0) {
             return 0;
@@ -22,6 +12,5 @@ public class Solution {
         return fib(N - 1) + fib(N - 2);
 
     }
-
 
 }
