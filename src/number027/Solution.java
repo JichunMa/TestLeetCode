ba7a8f09021ec76dd4edcbde6b329280d7b7005d
jichunMa <jichunma@yangqianguan.com>
2020-04-18 23:26:35 +0800

    移除元素

---
+++ src/number027/Solution.java
@@ -0,0 +1,14 @@
+package number027;
+
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
