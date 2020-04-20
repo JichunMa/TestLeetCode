a966789f7671af6b87386070cb3cb07906240ecc
jichunMa <jichunma@yangqianguan.com>
2020-04-20 23:34:02 +0800

    二叉树的最小深度

---
+++ src/number111/Solution.java
@@ -0,0 +1,20 @@
+package number111;
+
+import number098.TreeNode;
+
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return Math.min(left, right) + 1;
+    }
+}
